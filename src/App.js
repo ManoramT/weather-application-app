2470b5c52103725e5b3a138ffb76356d1f8793a8
manoramt <manoramtaparia@gmail.com>
2022-01-15 12:49:26 +0530

    calling upto cities api

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ const App = () => {
   const [cityNames, setCityNames] = useState([]);
   const [selectedCityName, setSelectedCityName] = useState("");
 
-  const getCountryNames = async () => {
+  useEffect(() => {
+    getCountryNames();
+  }, []);
+
+  const getCountryNames = () => {
     axios.get("https://www.universal-tutorial.com/api/getaccesstoken", { headers: {
       "Accept": "application/json",
       "api-token": "56yO7D4NrdRT7wqtVIn9p_XYA8pNKEZ77cz1QYAcKNfdCGbT4_DMo0xvxO7iWOu2kfI",
@@ -29,44 +33,59 @@ const App = () => {
     })
   };
 
-  const getStateNames = async() => {
+  const getStateNames = () => {
+    if(!selectedCountryName){
+      return;
+    }
     const authToken = localStorage.getItem('authToken');
-    axios.get("https://www.universal-tutorial.com/api/states/${}", { headers: {
+    axios.get(`https://www.universal-tutorial.com/api/states/${selectedCountryName}`, { headers: {
         "Accept": "application/json",
         "Authorization": `Bearer ${authToken}`
       }}).then((response) => {
         console.log(response);
-        setCountryNames(response.data);
+        setStateNames(response.data);
       })
   }
 
-  useEffect(() => {
-    getCountryNames();
-  }, []);
+  const getCityNames = () => {
+    if(!selectedStateName){
+      return;
+    }
+    const authToken = localStorage.getItem('authToken');
+    axios.get(`https://www.universal-tutorial.com/api/cities/${selectedStateName}`, { headers: {
+        "Accept": "application/json",
+        "Authorization": `Bearer ${authToken}`
+      }}).then((response) => {
+        console.log(response);
+        setCityNames(response.data);
+      })
+  }
 
   return (
     <div className="App">
-      <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" onInput={(e) => console.log(e.target.value)}/>
-      <datalist id="ice-cream-flavors">
+      <input list="country-list" id="country-choice" name="country-choice" onInput={(e) => setSelectedCountryName(e.target.value)}/>
+      <datalist id="country-list">
         {countryNames.map((countryObj, index) => {
           return <option key={index} value={countryObj.country_name} />
         })}
       </datalist>
-      <button type="button">OK</button>
+      <button type="button" onClick={getStateNames}>OK</button>
       <hr />
-      <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
-      <datalist id="ice-cream-flavors">
-        {countryNames.map((countryObj, index) => {
-          return <option key={index} value={countryObj.country_name} />
+      <input list="state-list" id="state-choice" name="state-choice" onInput={(e) => setSelectedStateName(e.target.value)}/>
+      <datalist id="state-list">
+        {stateNames.map((stateObj, index) => {
+          return <option key={index} value={stateObj.state_name} />
         })}
       </datalist>
+      <button type="button" onClick={getCityNames}>OK</button>
       <hr />
-      <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
-      <datalist id="ice-cream-flavors">
-        {countryNames.map((countryObj, index) => {
-          return <option key={index} value={countryObj.country_name} />
+      <input list="city-list" id="city-choice" name="city-choice" onInput={(e) => setSelectedCityName(e.target.value)}/>
+      <datalist id="city-list">
+        {cityNames.map((cityObj, index) => {
+          return <option key={index} value={cityObj.city_name} />
         })}
       </datalist>
+      <button type="button" onClick={()=> console.log(selectedCityName)}>OK</button>
     </div>
   );
 }
