0bcd84ad1d9bd55acf4458f00fc450f9e38da82b
manoramt <manoramtaparia@gmail.com>
2022-01-15 12:33:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [countryNames, setCountryNames] = useState([]);
+  const [selectedCountryName, setSelectedCountryName] = useState("");
+  const [stateNames, setStateNames] = useState([]);
+  const [selectedStateName, setSelectedStateName] = useState("");
+  const [cityNames, setCityNames] = useState([]);
+  const [selectedCityName, setSelectedCityName] = useState("");
+
+  const getCountryNames = async () => {
+    axios.get("https://www.universal-tutorial.com/api/getaccesstoken", { headers: {
+      "Accept": "application/json",
+      "api-token": "56yO7D4NrdRT7wqtVIn9p_XYA8pNKEZ77cz1QYAcKNfdCGbT4_DMo0xvxO7iWOu2kfI",
+      "user-email": "manoramtaparia@gmail.com"
+    }}).then((data) => {
+      const authToken = data.data.auth_token;
+      localStorage.setItem('authToken', authToken);
+      axios.get("https://www.universal-tutorial.com/api/countries/", { headers: {
+        "Accept": "application/json",
+        "Authorization": `Bearer ${authToken}`
+      }}).then((response) => {
+        console.log(response);
+        setCountryNames(response.data);
+      })
+    })
+  };
+
+  const getStateNames = async() => {
+    const authToken = localStorage.getItem('authToken');
+    axios.get("https://www.universal-tutorial.com/api/states/${}", { headers: {
+        "Accept": "application/json",
+        "Authorization": `Bearer ${authToken}`
+      }}).then((response) => {
+        console.log(response);
+        setCountryNames(response.data);
+      })
+  }
+
+  useEffect(() => {
+    getCountryNames();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" onInput={(e) => console.log(e.target.value)}/>
+      <datalist id="ice-cream-flavors">
+        {countryNames.map((countryObj, index) => {
+          return <option key={index} value={countryObj.country_name} />
+        })}
+      </datalist>
+      <button type="button">OK</button>
+      <hr />
+      <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
+      <datalist id="ice-cream-flavors">
+        {countryNames.map((countryObj, index) => {
+          return <option key={index} value={countryObj.country_name} />
+        })}
+      </datalist>
+      <hr />
+      <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
+      <datalist id="ice-cream-flavors">
+        {countryNames.map((countryObj, index) => {
+          return <option key={index} value={countryObj.country_name} />
+        })}
+      </datalist>
     </div>
   );
 }
